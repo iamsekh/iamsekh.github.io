471a12a52e54392a17b8da7c12b1e0ede2b96df9
Tahiruddin Sekh <72841469+iamsekh@users.noreply.github.com>
2024-07-07 11:57:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 (function () {
+    // Add event listeners for control buttons
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
-        })
+        });
     });
+
+    // Toggle light mode
     document.querySelector(".slider").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
+    });
+
+    // Smooth scrolling for navigation links
+    const navLinks = document.querySelectorAll('a[href^="#"]');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 })();
